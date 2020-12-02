9702265d5c84ca734a75feacf11d22fbedc58483
iakobi430 <73128513+iakobi430@users.noreply.github.com>
2020-12-02 19:33:21 +0400

    Add files via upload

---
+++ js/mouseover.js
@@ -0,0 +1,8 @@
+$('.grid-item1').mouseover(function() {
+    $('.ds').css("visibility","visible");
+  });
+  
+  $('.grid-itme1').mouseout(function() {
+    $('.ds').css("visibility","hidden");
+  });
+  
